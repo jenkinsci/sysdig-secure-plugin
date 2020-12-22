a7baa26b0352fa166dc6e8a95caf603dc32c9156
Alvaro Iradier <airadier@gmail.com>
2020-12-22 11:17:46 +0100

    Set inline scan disabled by default, for backwards compatibility

--- src/main/java/com/sysdig/jenkins/plugins/sysdig/SysdigBuilder.java
+++ src/main/java/com/sysdig/jenkins/plugins/sysdig/SysdigBuilder.java
@@ -154,7 +154,7 @@ public class SysdigBuilder extends Builder implements SimpleBuildStep {
     public static final String DEFAULT_NAME = "sysdig_secure_images";
     public static final boolean DEFAULT_BAIL_ON_FAIL = true;
     public static final boolean DEFAULT_BAIL_ON_PLUGIN_FAIL = true;
-    public static final boolean DEFAULT_INLINE_SCANNING = true;
+    public static final boolean DEFAULT_INLINE_SCANNING = false;
     public static final String DEFAULT_ENGINE_URL = "https://secure.sysdig.com";
     public static final boolean DEFAULT_ENGINE_VERIFY = true;
 
