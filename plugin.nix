043352a9b024e5faa7302c12332f0b6ce56bc832
Fede Barcelona <fede_rico_94@hotmail.com>
2024-10-01 17:27:26 +0200

    build(nix): update mvnHash

--- plugin.nix
+++ plugin.nix
@@ -4,7 +4,7 @@ maven.buildMavenPackage {
   pname = "sysdig-secure-plugin";
   version = "2.3.5";
   src = ./.;
-  mvnHash = "sha256-uJ86guTCEN/vmrBK5hKVIPeQtuOsWCX+NmdwJ54vMmk=";
+  mvnHash = "sha256-qDsvRzrNiTG9dHTOaVeu6hqfIOHV7ul8YMB14iRE38M=";
 
   doCheck = false;
   installPhase = "
