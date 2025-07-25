51663cacbfc2b782d22b6f6646860fd508ed43e6
Fede Barcelona <fede_rico_94@hotmail.com>
2025-07-25 17:23:36 +0200

    build(nix): remove nix plugin package

--- flake.nix
+++ flake.nix
@@ -11,41 +11,32 @@
       nixpkgs,
       flake-utils,
     }:
-
     let
-      overlays.default = final: prev: { plugin = final.callPackage ./plugin.nix { }; };
-
       setJavaVersion = final: prev: {
         jdk = prev.temurin-bin-17;
         jdt-language-server = prev.jdt-language-server.override { jdk = prev.jdk; };
       };
-
-      flake = flake-utils.lib.eachDefaultSystem (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              setJavaVersion
-              self.overlays.default
-            ];
-          };
-        in
-        {
-          packages = {
-            inherit (pkgs) plugin;
-            default = pkgs.plugin;
-          };
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              jdt-language-server
-              maven
-              jdk
-            ];
-          };
-          formatter = pkgs.nixfmt-rfc-style;
-        }
-      );
     in
-    flake // { inherit overlays; };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            setJavaVersion
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdt-language-server
+            maven
+            jdk
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+
 }
